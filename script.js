5f126d31f291ff2a810eb721d5701901fb627473
NafizImrose <nafizimrose.official@gmail.com>
2026-01-31 21:32:38 +0600

    add hide effect for hidden elements on scroll

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("show");
+//     }
+//   });
+// });
+
+// const elements = document.querySelectorAll(".hidden");
+
+// elements.forEach((el) => observer.observe(el));
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
     }
   });
 });
